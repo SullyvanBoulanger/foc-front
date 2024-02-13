4feb128c23adae5bd405adbd54a2b981e508d59c
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-13 10:25:44 +0100

    refacto: Form reviewed

--- src/containers/login.tsx
+++ src/containers/login.tsx
@@ -1,44 +1,54 @@
 import React, { ReactElement, useState } from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
+import { useForm } from "../utils/use-form";
+import { LabeledInput } from "../components/LabeledInput";
+
+interface LoginData{
+	usernameOrEmail: string;
+	password: string;
+}
 
 export function LoginPage(): ReactElement {
+	const {formState, handleInputChange, handleSubmit} = useForm<LoginData>({
+		usernameOrEmail: '',
+		password: ''
+	});
 	const [errorMessage, setErrorMessage] = useState<string>();
 	
-	const onSubmit = ((formData : React.SyntheticEvent) => {
-		formData.preventDefault();
-
-		const target = formData.target as typeof formData.target & {
-			usernameOrEmail: { value: string };
-			password: { value: string };
-		  };
-
-		const data = {
-			usernameOrEmail : target.usernameOrEmail.value, 
-			password : target.password.value
-		}
-
-		console.log(data);
-
+	const onSubmit = ((data: LoginData) => {
 		axios({
 			method: 'post',
 			url: 'http://localhost:8080/api/auth/signin',
 			data: data
+		}).then(response => {
+			if(response.status === 200){
+				// TODO
+				// navigate to
+			}
 		}).catch(reason => setErrorMessage("Your identifiants are incorrect."));
 	});
 
     return (
         <div>
-			<form onSubmit={onSubmit}>
-				<label>
-					Username Or Email
-					<input type="text" name="usernameOrEmail" />
-				</label>
-				<label>
-					Password
-					<input type="password" name="password"/>
-				</label>
-				<input type="submit" value="submit" />
+			<form onSubmit={(event) => handleSubmit(event, onSubmit)}>
+				<LabeledInput 
+					label="Username Or Email" 
+					name="usernameOrEmail" 
+					type="text" 
+					value={formState.usernameOrEmail} 
+					onChange={handleInputChange}
+				/>
+
+				<LabeledInput 
+					label="Password" 
+					name="password" 
+					type="password" 
+					value={formState.password} 
+					onChange={handleInputChange}
+				/>
+
+				<input type="submit" value="Log in" />
 			</form>
 			<br />
 			{errorMessage}
--- src/containers/register.tsx
+++ src/containers/register.tsx
@@ -1,81 +1,78 @@
-// import axios from "axios";
-// import { ReactElement, useState } from "react";
+import axios from "axios";
+import { ReactElement, useState } from "react";
+import { useForm } from "../utils/use-form";
+import { LabeledInput } from "../components/LabeledInput";
 
-import { ReactElement } from "react";
+type RegisterData = {
+	username : string
+    email : string
+	password : string
+    passwordConfirmation : string
+}
 
-// type FormData = {
-// 	username : string
-//     email : string
-// 	password : string
-//     passwordConfirmation : string
-// }
-
-// export function RegisterPage(): ReactElement {
-//     const {register, handleSubmit, watch, formState: {errors}} = useForm<FormData>();
-//     const [errorMessage, setErrorMessage] = useState<string>();
+export function RegisterPage(): ReactElement {
+    const {formState, handleSubmit, handleInputChange} = useForm<RegisterData>({
+        username : '',
+        email : '',
+        password : '',
+        passwordConfirmation : ''
+    });
+    const [errorMessage, setErrorMessage] = useState<string>();
 	
-// 	const onSubmit = handleSubmit(async (data) => {
-// 		console.log(data);
-//         const {passwordConfirmation, ...body} = data;
-//         axios({
-// 			method: 'post',
-// 			url: 'http://localhost:8080/api/auth/signup',
-// 			data: body
-// 		}).catch(reason => setErrorMessage(reason));
-// 	});
+	const onSubmit = (data: RegisterData) => {
+		console.log(data);
+        if(data.password !== data.passwordConfirmation){
+            setErrorMessage("Your passwords do no match")
+        }else{
+            setErrorMessage('');
+            const {passwordConfirmation, ...body} = data;
+            axios({
+                method: 'post',
+                url: 'http://localhost:8080/api/auth/signup',
+                data: body
+            }).catch(reason => setErrorMessage(reason));
+        }
+	};
 
-//     return (
-//         <div>
-// 			<form onSubmit={onSubmit}>
-// 				<label>
-// 					Username
-// 					<input type="text" {...register("username",{
-//                         required: "Username is required"
-//                     })} />
-//                     {errors.username && <p>{errors.username.message}</p>}
-// 				</label>
-//                 <br/>
-//                 <label>
-// 					Email
-// 					<input type="email" {...register("email",{
-//                         required: "Email is required"
-//                     })} />
-//                     {errors.email && <p>{errors.email.message}</p>}
-// 				</label>
-//                 <br/>
-// 				<label>
-// 					Password
-// 					<input type="password" {...register("password",{
-//                         required: "Password is required"
-//                     })} />
-//                     {errors.password && <p>{errors.password.message}</p>}
-// 				</label>
-//                 <br/>
-//                 <label>
-// 					Password confirmation
-// 					<input type="password" {...register("passwordConfirmation",{
-//                         required: true,
-//                         validate: (val: string) => {
-//                             if (watch('password') !== val) {
-//                                 return "Your passwords do no match"
-//                             }
-//                         }
-//                     })} />
-//                     {errors.passwordConfirmation && <p>{errors.passwordConfirmation.message}</p>}
-// 				</label>
-//                 <br/>
-// 				<input type="submit" value="Submit" />
-// 			</form>
-//             <br/>
-//             {errorMessage}
-//         </div>
-//     )
-// }
-
-export function RegisterPage(): ReactElement{
-    return(
-        <p>
-            Hell yeah
-        </p>
+    return (
+        <div>
+			<form onSubmit={(event) => handleSubmit(event, onSubmit)}>
+                <LabeledInput
+                    label="Username"
+                    type="text"
+                    name="username"
+                    value={formState.username}
+                    onChange={handleInputChange}
+                />
+                <br/>
+                <LabeledInput
+                    label="Email"
+                    type="text"
+                    name="email"
+                    value={formState.email}
+                    onChange={handleInputChange}
+                />
+                <br/>
+				<LabeledInput
+                    label="Password"
+                    type="password"
+                    name="password"
+                    value={formState.password}
+                    onChange={handleInputChange}
+                />
+                <br/>
+                <LabeledInput
+                    label="Password Confirmation"
+                    type="password"
+                    name="passwordConfirmation"
+                    value={formState.passwordConfirmation}
+                    onChange={handleInputChange}
+                />
+                <br/>
+				<input type="submit" value="Register" />
+			</form>
+            <br/>
+            {errorMessage}
+        </div>
     )
 }
