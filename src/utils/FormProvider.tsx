ff0581703dfbf88bc662394c4bc7eafc65bec5ab
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-14 14:44:09 +0100

    refacto: Apply PR advices

---
+++ src/utils/FormProvider.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren, ReactElement, createContext, useMemo, useState } from "react";
+import { PrimitiveTypes } from "./UseForm";
+
+interface FormContextType{
+    formState : Record<string, PrimitiveTypes>;
+    setFormState : (newValue: Record<string, PrimitiveTypes>) => void
+}
+
+export const FormContext = createContext<FormContextType>({
+    formState: {},
+    setFormState: ({}) => {}
+});
+
+export default function FormProvider({children}: PropsWithChildren): ReactElement{
+    const [formState, setFormState] = useState<Record<string, PrimitiveTypes>>();
+
+    const providedValues = useMemo(() => {
+        return { formState: formState || {}, setFormState };
+    }, [formState, setFormState]);
+
+    return(
+        <FormContext.Provider value={providedValues}>
+            {children}
+        </FormContext.Provider>
+    );
+}
