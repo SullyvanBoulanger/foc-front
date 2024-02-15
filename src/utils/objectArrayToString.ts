7f72b3c703832b57d35b897a00c2ee2318ff817b
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-15 11:43:27 +0100

    refacto: Apply PR advices

--- src/utils/objectArrayToString.ts
+++ src/utils/objectArrayToString.ts
@@ -2,14 +2,5 @@ export default function objectArrayToString<T>(
   array: T[],
   propertyName : string,
 ): string {
-  let stringBuilded = '';
-
-  array.forEach((value, index) => {
-    stringBuilded += value[propertyName as keyof T];
-    if (index !== array.length - 1) {
-      stringBuilded += ', ';
-    }
-  });
-
-  return stringBuilded;
+  return array.map((obj) => obj[propertyName as keyof T]).join(', ');
 }
