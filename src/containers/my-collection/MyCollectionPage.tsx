271c4e5b3434ae1aa02f77b278375f027464fbb5
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-19 11:43:56 +0100

    refacto: Apply PR advices

--- src/containers/my-collection/MyCollectionPage.tsx
+++ src/containers/my-collection/MyCollectionPage.tsx
@@ -1,27 +1,14 @@
 import { UserCardPreview } from '@models/Card';
 import React, { ReactElement } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import CardPreviewComponent from '@components/CardPreview';
+import StackUserCard from './StackUserCard';
 
 export default function MyCollectionPage(): ReactElement {
   const cards = useLoaderData() as UserCardPreview[];
 
-  const stackUserCard = ({ id, url_picture, quantity }: UserCardPreview) => {
-    const cardsPreviews = [];
-    for (let i = 0; i < quantity; i += 1) {
-      cardsPreviews.push(
-        <div className="p-1">
-          <CardPreviewComponent id={id} url_picture={url_picture} />
-        </div>,
-      );
-    }
-
-    return cardsPreviews;
-  };
-
   return (
-    <div className="flex">
-      {cards.map((card) => stackUserCard(card))}
+    <div className="flex flex-wrap">
+      {cards.map((card) => <StackUserCard {...card} />)}
     </div>
   );
 }
