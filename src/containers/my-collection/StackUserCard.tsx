fb7346d701c7f671678f3ab1ac0006b224ed3101
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-19 12:16:40 +0100

    refacto: Apply PR advices

--- src/containers/my-collection/StackUserCard.tsx
+++ src/containers/my-collection/StackUserCard.tsx
@@ -3,18 +3,16 @@ import { UserCardPreview } from '@models/Card';
 import React, { ReactElement } from 'react';
 
 export default function StackUserCard({ quantity, ...card }: UserCardPreview): ReactElement {
-  const cardsPreviews = [];
-  for (let i = 0; i < quantity; i += 1) {
-    cardsPreviews.push(
-      <div className="p-1">
-        <CardPreviewComponent {...card} />
-      </div>,
-    );
-  }
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
     <>
-      { cardsPreviews }
+      {[...Array(quantity)].map(
+        () => (
+          <div className="p-1">
+            <CardPreviewComponent {...card} />
+          </div>
+        ),
+      )}
     </>
   );
 }
