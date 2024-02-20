0e2696acb4b0d93cfcc0daf7024275a849acac0f
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-20 13:13:17 +0100

    refacto: Apply PR advices for clsx

--- src/nectron/Label.tsx
+++ src/nectron/Label.tsx
@@ -1,12 +1,14 @@
+import clsx from 'clsx';
 import React, { DetailedHTMLProps, LabelHTMLAttributes, PropsWithChildren } from 'react';
 
 export default function Label({
   children,
+  className,
   ...props
 }: PropsWithChildren<DetailedHTMLProps<LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement>>) {
   return (
     // eslint-disable-next-line jsx-a11y/label-has-associated-control
-    <label className="block mb-2 text-sm font-medium text-gray-900" {...props}>
+    <label className={clsx(className, 'block mb-2 text-sm font-medium text-gray-900')} {...props}>
       {children}
     </label>
   );
