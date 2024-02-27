e02e4333b61d8fa7e29191223982ba28227b8b27
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-27 08:58:21 +0100

    fix: Search was not put to undefined, blocking the entire website

--- src/components/CardPreview.tsx
+++ src/components/CardPreview.tsx
@@ -1,10 +1,13 @@
+import { SearchContext } from '@containers/layout/Layout';
 import { CardPreview } from '@models/Card';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function CardPreviewComponent({ id, url_picture }: CardPreview): ReactElement {
+  const emptySearchResult = useContext(SearchContext);
+
   return (
-    <NavLink to={`/card/${id}`}>
+    <NavLink to={`/card/${id}`} onClick={emptySearchResult}>
       <img src={url_picture} alt="CardPicture" className="w-72" />
     </NavLink>
   );
