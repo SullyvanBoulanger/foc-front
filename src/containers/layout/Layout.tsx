e115461ccc3052ae61a0ee708748cf555352d294
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-27 08:58:21 +0100

    fix: Search was not put to undefined, blocking the entire website

--- src/containers/layout/Layout.tsx
+++ src/containers/layout/Layout.tsx
@@ -1,26 +1,34 @@
 import { CardPreview } from '@models/Card';
 import Button from '@nectron/Button';
-import React, { ReactElement, useState } from 'react';
+import React, {
+  ReactElement, createContext, useMemo, useState,
+} from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import ShowResult from './ShowResult';
 
+export const SearchContext = createContext<() => void>(() => null);
+
 export default function Layout(): ReactElement {
   const [searchResult, setSearchResult] = useState<CardPreview[]>();
 
+  const emptySearchResult = useMemo(() => () => setSearchResult(undefined), []);
+
   return (
     <div>
-      <div className="border-b-4 border-grey flex justify-between items-center h-1/10 w-screen p-2 box-border">
-        <NavLink to="/my_collection" onClick={() => setSearchResult(undefined)}>
-          My collection
-        </NavLink>
-        <SearchBar setResult={setSearchResult} />
-        <Button>
-          Logout
-        </Button>
-      </div>
-      {!searchResult && <Outlet />}
-      <ShowResult result={searchResult} />
+      <SearchContext.Provider value={emptySearchResult}>
+        <div className="border-b-4 border-grey flex justify-between items-center h-1/10 w-screen p-2 box-border">
+          <NavLink to="/my_collection" onClick={emptySearchResult}>
+            My collection
+          </NavLink>
+          <SearchBar setResult={setSearchResult} />
+          <Button>
+            Logout
+          </Button>
+        </div>
+        {!searchResult && <Outlet />}
+        <ShowResult result={searchResult} />
+      </SearchContext.Provider>
     </div>
   );
 }
