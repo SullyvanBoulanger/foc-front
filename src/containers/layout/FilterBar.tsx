f8d4f4f3ee1ebf799d5ac0fc37a5e3212c578cf0
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-28 09:06:09 +0100

    feat: Implement Filter Search

---
+++ src/containers/layout/FilterBar.tsx
@@ -0,0 +1,88 @@
+import SelectForm from '@components/SelectForm';
+import Label from '@nectron/Label';
+import { api } from '@utils/api';
+import React, {
+  ReactElement, useEffect, useRef, useState,
+} from 'react';
+import { Filters } from '@models/Filters';
+import { useForm } from '@utils/UseForm';
+import { PrimitiveTypes } from '@utils/PrimitiveTypes';
+import Input from '@nectron/Input';
+import InputStatForm from './InputStatForm';
+
+interface FilterBarProps {
+  isFilterVisible : boolean;
+  debounceSubmit : (data: Record<string, PrimitiveTypes>) => void;
+}
+
+export function useFirstRender() {
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    firstRender.current = false;
+  }, []);
+
+  return firstRender.current;
+}
+
+export default function FilterBar({
+  isFilterVisible,
+  debounceSubmit,
+}: FilterBarProps): ReactElement {
+  const [filters, setFilters] = useState<Filters>();
+  const { formState, setFormState } = useForm();
+  const firstRender = useFirstRender();
+
+  const setUserCollection = (event: React.MouseEvent<HTMLInputElement>) => {
+    const isChecked = (event.target as HTMLInputElement).checked;
+    const result = isChecked ? 'user' : '';
+    setFormState({ ...formState, collection: result });
+  };
+
+  useEffect(() => {
+    api.get('/card/filters').then((response) => setFilters(response.data));
+  }, []);
+
+  useEffect(() => {
+    if (firstRender) return;
+
+    debounceSubmit(formState);
+  }, [formState]);
+
+  return (
+    <div hidden={!isFilterVisible} className="absolute flex flex-wrap justify-center items-center mt-10 w-1/2 border-grey bg-silver border-4 p-2 rounded mx-auto left-1/2 transform -translate-x-1/2 gap-2">
+      <Label>
+        User Collection
+        <Input name="collection" id="collection" type="checkbox" onClick={setUserCollection} />
+      </Label>
+      <Label>
+        Atk
+        <InputStatForm name="atk" placeholder="Atk..." />
+      </Label>
+      <Label>
+        Def
+        <InputStatForm name="def" placeholder="Def..." />
+      </Label>
+      <Label>
+        Type(s)
+        <SelectForm multiple name="types" options={filters?.types.map((type) => type.name) || []} />
+      </Label>
+      <Label>
+        Attribute(s)
+        <SelectForm multiple name="attributes" options={filters?.attributes.map((attribute) => attribute.name) || []} />
+      </Label>
+      <Label>
+        Race/Trait(s)
+        <SelectForm multiple name="raceTraits" options={filters?.raceTraits.map((raceTrait) => raceTrait.name) || []} />
+      </Label>
+      <Label>
+        Set(s)
+        <SelectForm multiple name="sets" options={filters?.sets.map((set) => set.abbreviation) || []} />
+      </Label>
+      <Label>
+        Rarity(ies)
+        <SelectForm multiple name="rarities" options={filters?.rarities.map((rarity) => rarity.code) || []} />
+      </Label>
+    </div>
+  );
+}
--- src/containers/layout/SearchBar.tsx
+++ src/containers/layout/SearchBar.tsx
@@ -1,18 +1,15 @@
 import Form from '@components/Form';
 import InputForm from '@components/InputForm';
-import SelectForm from '@components/SelectForm';
 import { TCardPreview } from '@models/Card';
-import { Filters } from '@models/Filters';
 import Button from '@nectron/Button';
-import Label from '@nectron/Label';
 import { PrimitiveTypes } from '@utils/PrimitiveTypes';
 import { api } from '@utils/api';
 import { debounce } from '@utils/debounce';
 import React, {
   ChangeEvent,
-  Dispatch, ReactElement, SetStateAction, useEffect, useState,
+  Dispatch, ReactElement, SetStateAction, useState,
 } from 'react';
-import InputStatForm from './InputStatForm';
+import FilterBar from './FilterBar';
 
 interface SearchBarProps {
   setResult : Dispatch<SetStateAction<TCardPreview[] | undefined>>
@@ -20,7 +17,6 @@ interface SearchBarProps {
 
 export default function SearchBar({ setResult } : SearchBarProps): ReactElement {
   const [isFilterVisible, setIsFilterVisible] = useState(false);
-  const [filters, setFilters] = useState<Filters>();
 
   const constructQuery = (data: Record<string, PrimitiveTypes>): string => {
     let query = '';
@@ -56,10 +52,6 @@ export default function SearchBar({ setResult } : SearchBarProps): ReactElement 
     debounceSearchNameCall(currentValue);
   });
 
-  useEffect(() => {
-    api.get('/card/filters').then((response) => setFilters(response.data));
-  }, []);
-
   return (
     <Form handleSubmit={onSubmit}>
       <div className="flex gap-1">
@@ -83,42 +75,7 @@ export default function SearchBar({ setResult } : SearchBarProps): ReactElement 
             <line x1="4" y1="18" x2="16" y2="18" />
           </svg>
         </Button>
-        <div hidden={!isFilterVisible} className="absolute flex flex-wrap justify-center items-center mt-10 w-1/2 border-grey bg-silver border-4 p-2 rounded mx-auto left-1/2 transform -translate-x-1/2 gap-2">
-          {/* <Label>
-            Collection
-            <Input name="collection" id="collection" type="checkbox" />
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control
-            <label htmlFor="collection"> User </label>
-          </Label> */}
-          <Label>
-            Atk
-            <InputStatForm name="atk" placeholder="Atk..." />
-          </Label>
-          <Label>
-            Def
-            <InputStatForm name="def" placeholder="Def..." />
-          </Label>
-          <Label>
-            Type(s)
-            <SelectForm multiple name="types" options={filters?.types.map((type) => type.name) || []} />
-          </Label>
-          <Label>
-            Attribute(s)
-            <SelectForm multiple name="attributes" options={filters?.attributes.map((attribute) => attribute.name) || []} />
-          </Label>
-          <Label>
-            Race/Trait(s)
-            <SelectForm multiple name="raceTraits" options={filters?.raceTraits.map((raceTrait) => raceTrait.name) || []} />
-          </Label>
-          <Label>
-            Set(s)
-            <SelectForm multiple name="sets" options={filters?.sets.map((set) => set.abbreviation) || []} />
-          </Label>
-          <Label>
-            Rarity(ies)
-            <SelectForm multiple name="rarities" options={filters?.rarities.map((rarity) => rarity.code) || []} />
-          </Label>
-        </div>
+        <FilterBar debounceSubmit={onSubmit} isFilterVisible={isFilterVisible} />
       </div>
     </Form>
   );
